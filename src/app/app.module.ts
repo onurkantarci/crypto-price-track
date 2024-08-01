e8606d52d5058092930b697bdfbf50d5fb01b4b8
onurkantarci <onurkntrc@gmail.com>
2024-08-01 21:08:35 +0300

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { CoinListComponent } from './coin-list/coin-list.component';
+import { CoinDetailComponent } from './coin-detail/coin-detail.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { BaseChartDirective } from 'ng2-charts';
+
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CoinListComponent, CoinDetailComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    BaseChartDirective,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideCharts(withDefaultRegisterables()),
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/coin-list/coin-list.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Router } from '@angular/router';
+import { CurrencyService } from '../service/currency.service';
+
+@Component({
+  selector: 'app-coin-list',
+  templateUrl: './coin-list.component.html',
+  styleUrl: './coin-list.component.scss',
+})
+export class CoinListComponent implements OnInit {
+  bannerData: any = [];
+  currency: string = 'USD';
+  dataSource!: MatTableDataSource<any>;
+
+  displayedColumns: string[] = [
+    'symbol',
+    'current_price',
+    'price_change_percentage_24h',
+    'market_cap',
+  ];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private api: ApiService,
+    private router: Router,
+    private currencyService: CurrencyService
+  ) {}
+
+  ngOnInit(): void {
+    this.getAllData();
+    this.getBannerData();
+    this.currencyService.getCurrency().subscribe((val) => {
+      this.currency = val;
+      this.getAllData();
+      this.getBannerData();
+    });
+  }
+
+  getBannerData() {
+    this.api.getTrendingCurrency(this.currency).subscribe((res) => {
+      console.log(res);
+      this.bannerData = res;
+    });
+  }
+
+  getAllData() {
+    this.api.getCurrency(this.currency).subscribe((res) => {
+      console.log(res);
+      this.dataSource = new MatTableDataSource(res);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  gotoDetails(row: any) {
+    this.router.navigate(['coin-detail', row.id]);
+  }
+}
